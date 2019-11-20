bd366bbd6f2aeb0dae3403bb794ed8e9e00eb0c8
Kaycee Tate <40043924+KayTate@users.noreply.github.com>
2019-11-20 13:11:53 -0600

    Added some game logic

--- app/src/main/java/com/example/scarnesdice/MainActivity.java
+++ app/src/main/java/com/example/scarnesdice/MainActivity.java
@@ -5,11 +5,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.time.temporal.TemporalQueries;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,17 +26,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button roll = findViewById(R.id.roll_btn);
-        roll.setOnClickListener(new RollListener());
+        Button roll_btn = findViewById(R.id.roll_btn);
+        roll_btn.setOnClickListener(new RollListener());
 
-//        Button reset = findViewById(R.id.reset_btn);
-//        reset.setOnClickListener(new ResetListener());
-//
-//        Button hold = findViewById(R.id.hold_btn);
-//        hold.setOnClickListener(new HoldListener());
+        Button reset_btn = findViewById(R.id.reset_btn);
+        reset_btn.setOnClickListener(new ResetListener());
+
+        Button hold_btn = findViewById(R.id.hold_btn);
+        hold_btn.setOnClickListener(new HoldListener());
     }
 
-    private int gameTurn(int roll){
+    private int gameTurn(String user) {
         Log.w(APP_TAG, String.format("Top of gameturn method"));
         int turn_score;
 
@@ -55,35 +53,35 @@ public class MainActivity extends AppCompatActivity {
     private class RollListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            roll = rand.nextInt(6) + 1;
-            ImageView dice = findViewById(R.id.dice_view);
-            Log.w(APP_TAG, String.format("Rolled a %d", roll));
+            Roll();
 
-            switch (roll) {
-                case 1:
-                    dice.setImageResource(R.drawable.dice1);
-                    break;
-                case 2:
-                    dice.setImageResource(R.drawable.dice2);
-                    break;
-                case 3:
-                    dice.setImageResource(R.drawable.dice3);
-                    break;
-                case 4:
-                    dice.setImageResource(R.drawable.dice4);
-                    break;
-                case 5:
-                    dice.setImageResource(R.drawable.dice5);
-                    break;
-                case 6:
-                    dice.setImageResource(R.drawable.dice6);
-                    break;
-            }
+        }
+    }
 
-            UserTurn = gameTurn(roll);
-            TextView turn_score_text = findViewById(R.id.turn_score_textView);
-            turn_score_text.setText(String.format("Turn Score: %d", UserTurn));
+    private void Roll() {
+        roll = rand.nextInt(6) + 1;
+        ImageView dice = findViewById(R.id.dice_view);
+        Log.w(APP_TAG, String.format("Rolled a %d", roll));
 
+        switch (roll) {
+            case 1:
+                dice.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                dice.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                dice.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                dice.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                dice.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                dice.setImageResource(R.drawable.dice6);
+                break;
         }
     }
 
@@ -91,13 +89,19 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View view) {
+            Hold("user");
 
-            UserTurn = gameTurn(roll);
-            UserOverall += UserTurn;
+        }
+    }
 
-            TextView user_score = findViewById(R.id.user_score_textview);
-            user_score.setText(String.format("Your Score: %d", UserOverall));
-
+    private void Hold(String user) {
+        switch (user) {
+            case "computer":
+                CompOverall += CompTurn;
+                break;
+            case "user":
+                UserOverall += UserTurn;
+                break;
         }
     }
 
