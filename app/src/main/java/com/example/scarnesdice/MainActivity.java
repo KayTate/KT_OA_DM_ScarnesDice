6a95a835693ae3693facd324b47b4798541e3ccf
Kaycee Tate <40043924+KayTate@users.noreply.github.com>
2019-11-21 11:14:27 -0600

    Cleaned file

--- app/src/main/java/com/example/scarnesdice/MainActivity.java
+++ app/src/main/java/com/example/scarnesdice/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,46 +26,80 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w(APP_TAG, "Content view set");
 
         Button roll_btn = findViewById(R.id.roll_btn);
         roll_btn.setOnClickListener(new RollListener());
-
+        Button hold_btn = findViewById(R.id.hold_btn);
+        hold_btn.setOnClickListener(new HoldListener());
         Button reset_btn = findViewById(R.id.reset_btn);
         reset_btn.setOnClickListener(new ResetListener());
 
-        Button hold_btn = findViewById(R.id.hold_btn);
-        hold_btn.setOnClickListener(new HoldListener());
+        String turn_score_string = getString(R.string.turn_score);
+        String user_score_string = getString(R.string.user_score);
+        String comp_score_stirng = getString(R.string.comp_score);
+
+        TextView turn_score = findViewById(R.id.turn_score_textView);
+        turn_score.setText(String.format(Locale.US, "%s %d", turn_score_string, 0));
+        TextView user_score = findViewById(R.id.user_score_textview);
+        user_score.setText(String.format(Locale.US, "%s %d", user_score_string, 0));
+        TextView computer_score = findViewById(R.id.comp_score_textView);
+        computer_score.setText(String.format(Locale.US, "%s %d", comp_score_stirng, 0));
     }
 
 
+    //Turns
     private void userTurn(int roll) {
-        if(roll != 1){
+        if (roll != 1) {
             UserTurn += roll;
-        }
-        else{
+        } else {
             UserTurn = 0;
             Hold();
         }
     }
 
+    private void ComputerTurn() {
+        Log.w(APP_TAG, "Computer Turn");
 
-    private class RollListener implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            Log.w(APP_TAG, "User turn");
+        Button roll_btn = findViewById(R.id.roll_btn);
+        roll_btn.setEnabled(false);
+        Button hold_btn = findViewById(R.id.hold_btn);
+        hold_btn.setEnabled(false);
 
+        TextView turn_score = findViewById(R.id.turn_score_textView);
+        TextView computer_score = findViewById(R.id.comp_score_textView);
+
+        String turn_score_string = getString(R.string.turn_score);
+        String comp_score_stirng = getString(R.string.comp_score);
+
+        while (CompTurn < 20) {
             int roll = Roll();
-            userTurn(roll);
-
-            TextView turn_score = findViewById(R.id.turn_score_textView);
-            turn_score.setText(String.format("Turn Score: %d", UserTurn));
-
+            //Good place to insert wait?
+            if (roll != 1) {
+                CompTurn += roll;
+            } else {
+                CompTurn = 0;
+                break;
+            }
+            turn_score.setText(String.format(Locale.US, "%s %d", turn_score_string, CompTurn));
         }
+
+        CompOverall += CompTurn;
+        CompTurn = 0;
+        Log.w(APP_TAG, "Comp overall " + CompOverall);
+
+        computer_score.setText(String.format(Locale.US, "%s %d", comp_score_stirng, CompOverall));
+        turn_score.setText(String.format(Locale.US, "%s %d", turn_score_string, 0));
+
+        roll_btn.setEnabled(true);
+        hold_btn.setEnabled(true);
+
     }
 
+    //Helpers
     private int Roll() {
-        int roll = rand.nextInt(6) + 1;
         ImageView dice = findViewById(R.id.dice_view);
+        int roll = rand.nextInt(6) + 1;
         Log.w(APP_TAG, String.format("Rolled a %d", roll));
 
         switch (roll) {
@@ -91,7 +126,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private class HoldListener implements View.OnClickListener{
+    private void Hold() {
+        UserOverall += UserTurn;
+        UserTurn = 0;
+
+        String turn_score_string = getString(R.string.turn_score);
+        String user_score_string = getString(R.string.user_score);
+
+        TextView user_score = findViewById(R.id.user_score_textview);
+        user_score.setText(String.format(Locale.US, "%s %d", user_score_string, UserOverall));
+        TextView turn_score = findViewById(R.id.turn_score_textView);
+        turn_score.setText(String.format(Locale.US, "%s %d", turn_score_string, 0));
+
+        ComputerTurn();
+    }
+
+
+    //Listeners
+    private class RollListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Log.w(APP_TAG, "User turn");
+
+            int roll = Roll();
+            userTurn(roll);
+
+            String turn_score_string = getString(R.string.turn_score);
+
+            TextView turn_score = findViewById(R.id.turn_score_textView);
+            turn_score.setText(String.format(Locale.US, "%s %d", turn_score_string, UserTurn));
+
+        }
+    }
+
+    private class HoldListener implements View.OnClickListener {
 
         @Override
         public void onClick(View view) {
@@ -100,20 +168,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void Hold() {
-        UserOverall += UserTurn;
-        UserTurn = 0;
-
-        TextView user_score = findViewById(R.id.user_score_textview);
-        user_score.setText(String.format("Your Score: %d", UserOverall));
-
-        TextView turn_score = findViewById(R.id.turn_score_textView);
-        turn_score.setText("Turn Score: 0");
-
-        ComputerTurn();
-    }
-
-    private class ResetListener implements View.OnClickListener{
+    private class ResetListener implements View.OnClickListener {
 
         @Override
         public void onClick(View view) {
@@ -123,39 +178,5 @@ public class MainActivity extends AppCompatActivity {
             CompTurn = 0;
 
         }
-    }
-
-    private void ComputerTurn() {
-        Log.w(APP_TAG, "Computer Turn");
-        Button roll_btn = findViewById(R.id.roll_btn);
-        Button hold_btn = findViewById(R.id.hold_btn);
-
-        roll_btn.setEnabled(false);
-        hold_btn.setEnabled(false);
-
-        while (CompTurn < 20) {
-            int roll = Roll();
-            if (roll != 1) {
-                CompTurn += roll;
-            } else {
-                CompTurn = 0;
-                break;
-            }
-            TextView turn_score = findViewById(R.id.turn_score_textView);
-            turn_score.setText(String.format("Turn Score: %d", CompTurn));
-        }
-        CompOverall += CompTurn;
-        CompTurn = 0;
-        Log.w(APP_TAG, "Comp overall " + CompOverall);
-
-        TextView computer_score = findViewById(R.id.comp_score_textView);
-        computer_score.setText(String.format("Computer Score: %d", CompOverall));
-
-        TextView turn_score = findViewById(R.id.turn_score_textView);
-        turn_score.setText("Turn Score: 0");
-
-        roll_btn.setEnabled(true);
-        hold_btn.setEnabled(true);
-
     }
 }
